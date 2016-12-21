617e978102f0b47a57d8beb5b4023d896ad5c625
NiraBH <nirabethencourt@gmail.com>
2016-12-21 19:19:43 +0100

    Cifrado

--- src/edu/femxa/val/herencia/Cesar.java
+++ src/edu/femxa/val/herencia/Cesar.java
@@ -7,12 +7,14 @@ public class Cesar {
 		// TODO Auto-generated method stub
 		Cesar cesar = null; // Clase variable = null; 
 		String cadena;		
+		
 			cesar = new Cesar(); //variable = new Objeto/metodo () que se llamara igual que la clase.
-			cadena = cesar.cifrarCesar("POLLOLOCO", 4);
+			
+			cadena = cesar.cifrarCesar("HE COMIDO CARNE", 3);
 			
 			System.out.println(cadena);
 			
-//	int  j =81;
+//	int  j =81; para llamarlo al reves de numero a letra.
 //	char c= (char)j;
 //	System.out.println(c);
 	}
@@ -35,35 +37,39 @@ public class Cesar {
 	
 	public String cifrarCesar (String palabra, int clave)
 	{
+		String palabra_cifrada = null;
 		int longi = palabra.length();
 		//int numero_carac = 0;
-		char caracter = 0;
+		char caracter_actual = 0;
 		int caracter_n = 0; 
 		char caracter_cifrado = 0;
-		String cadena_dev = "";
 		
+		palabra_cifrada = new String();
+		palabra_cifrada = palabra_cifrada + "";	
 		
 	
 			for (int pos = 0; pos < longi; pos++)
 				{
+				caracter_actual = palabra.charAt(pos);
 				
-				if (caracter == ' ')
+				//palabra_cifrada =  palabra_cifrada + ' ';
+				//numero_carac = (int) caracter;
+							
+				
+				
+				if (caracter_actual != ' ')
 				{
-					cadena_dev = cadena_dev + ' ';
+					
+					caracter_n = (int) caracter_actual; //habría que hacer una conversion primero a numero int
+					caracter_n = (caracter_n + clave); //para luego hacer una suma de numeros y no sumar letras y numeros
+					caracter_cifrado =  (char) caracter_n;
+					palabra_cifrada = palabra_cifrada + caracter_cifrado;
 				}	
-				else 
-				{
-					caracter = palabra.charAt(pos);
-					caracter_n = (int) caracter; //habría que hacer una conversion primero a numero int
-					
-					//caracter_cifrado = (char) (caracter_n + clave); //para luego hacer una suma de numeros y no sumar letras y numeros
-					
-					//numero_carac = (int) caracter;
-								
-					cadena_dev =  cadena_dev + caracter;
-						}
+			
+				else  {palabra_cifrada =  palabra_cifrada + caracter_actual;}
 				}
-			return cadena_dev;
+			
+			return palabra_cifrada;
 		
 			
 			
