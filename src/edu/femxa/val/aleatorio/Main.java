39e3230a5d756617619bdb01363d3589ea11aa89
NiraBH <nirabethencourt@gmail.com>
2016-12-22 21:16:53 +0100

    Juego Azar

---
+++ src/edu/femxa/val/aleatorio/Main.java
@@ -0,0 +1,14 @@
+package edu.femxa.val.aleatorio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//creas objeto para llamar random
+		
+		
+		
+		
+	}
+
+}
