024beaced9b239838c371f460ef2ff828f302692
NiraBH <nirabethencourt@gmail.com>
2017-01-09 18:06:14 +0100

    Palíndromo

---
+++ src/edu/femxa/val/palindromo/SiPalindromo.java
@@ -0,0 +1,50 @@
+package edu.femxa.val.palindromo;
+
+public class SiPalindromo {
+	
+	String frase_introducida;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cad = null;
+		
+		cad = "dabalearrozalazorraelabad";
+		esPalindromo(cad);
+		
+		
+		
+	}
+
+	public static boolean esPalindromo (String cad)
+	{
+		boolean bes_palim = false;
+		int letra_inicial = 0;
+		int letra_final = cad.length()-1;
+		
+		    
+		    
+			while ((letra_inicial < letra_final) && (!bes_palim))
+			{
+				if (cad.charAt(letra_inicial) == cad.charAt(letra_final))
+				{
+					letra_inicial ++;
+					letra_final --;
+				}
+				else bes_palim = true;
+			}
+			
+		
+			if (!bes_palim)
+			{
+				System.out.println("TODO ESTO ES PALINDROMO");
+				}
+			else 
+			{
+				System.out.println("No lo es... ");
+			}
+		return bes_palim;
+		
+	}
+}
+
