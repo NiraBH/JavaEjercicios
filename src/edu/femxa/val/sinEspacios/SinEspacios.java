ea14698fca9cb678e4957d8f4f00310a839be724
nirabethencourt <nirabethencourt@gmail.com>
2017-01-11 16:06:11 +0100

    Dos metodos

--- src/edu/femxa/val/sinEspacios/SinEspacios.java
+++ src/edu/femxa/val/sinEspacios/SinEspacios.java
@@ -4,27 +4,53 @@ import java.util.Scanner;
 
 public class SinEspacios {
 
+	
 	public static void main(String[] args) {
 		
-		String cadena = null;
-		String cadena_sin_espacios = null;
+		SinEspacios palabra =  new SinEspacios();
+		String palabra_sin_espacios = palabra.pedirPalabra();
 		
+		SinEspacios sinEspacios = new SinEspacios();
+		String palabra_devuelta = sinEspacios.quitarEspacios(palabra_sin_espacios);
+		
+		System.out.println("Su frase sin espacios es: " +palabra_devuelta);
+	
+		
+	}
+	
+	public static String pedirPalabra()
+		{
+		String cadena = null;
 		Scanner scanner = new Scanner (System.in); 
 		
 		System.out.println("Por favor, introduzca su frase:");
 		
-		cadena= scanner.nextLine();
+		cadena = scanner.nextLine();
 		
+		return cadena;
+		
+		}
+	public static String quitarEspacios (String cadena) 
+	{
+		String cadena_sin_espacios = "";
 		
 		for(int i=0; i< cadena.length(); i++)
 			{
 			if(cadena.charAt(i) != ' ')
 			{
-				cadena_sin_espacios += cadena.charAt(i);
-							
+				
+				cadena_sin_espacios = cadena_sin_espacios + cadena.charAt(i); // es una manera de hacerlo como si fuera i++ cadena_sin_espacios += cadena.charAt(i);
+				
+			}
+			
+			
 				}
+		return cadena_sin_espacios;
+		
 				
 	}
-		System.out.println(cadena_sin_espacios);
-}
-}
+		
+
+
+		
+	}
