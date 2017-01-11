ae64226996bd343b7c460c549b2a66bfb56216e3
nirabethencourt <nirabethencourt@gmail.com>
2017-01-11 21:22:48 +0100

    casi..

--- src/edu/femxa/val/file/MainArchivos.java
+++ src/edu/femxa/val/file/MainArchivos.java
@@ -7,28 +7,36 @@ public class MainArchivos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File archivo = null;
-		archivo = new File("Foldercito");
-		boolean existe = archivo.exists();
+		archivo = new File("/Users/nirabethencourt/Documents/workspace/edu.femxa.val.ejernavi");
+		//boolean existe = archivo.exists();
 		
 		
-		if (existe)
-			{
-			System.out.println("Existe el directorio");
+		boolean si_es_carpeta = archivo.isDirectory();
+		
+		while (si_es_carpeta)
+		{
+			String nombre = archivo.getName();
 			String [] lista = null;
 			lista = archivo.list();
 			
-			for (int i=0; i < lista.length; i++)
-			{
-				boolean esta_oculto = archivo.isHidden();
-				
-				if (esta_oculto)
-				System.out.println(lista [i]);
-			}
+			System.out.println();
 		}
-		else
-		{
-			System.out.println("No existe el directorio");
-		}
+		
+//		if (existe)
+//			{
+//			System.out.println("Existe el directorio y los documentos son: \n ");
+//			String [] lista = null;
+//			lista = archivo.list();
+//			
+//			for (int i=0; i < lista.length; i++)
+//			{
+//				System.out.println(lista[i]);
+//			}
+//		}
+//		else
+//		{
+//			System.out.println("No existe el directorio");
+//		}
 		
 		
 		
